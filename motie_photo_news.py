2b4a303dc036bf85fc0520dcb869c5aeef847fe6
unknown <nfranceshealy@gmail.com>
2018-03-05 16:08:39 +0000

    more changes

--- motie_photo_news.py
+++ motie_photo_news.py
@@ -1,3 +1,9 @@
+
+# coding: utf-8
+
+# In[21]:
+
+
 # Import BeautifulSoup library
 from bs4 import BeautifulSoup
 
@@ -7,10 +13,20 @@ import urllib2
 # Import Pandas
 import pandas as pd
 
+
+# In[22]:
+
+
+date_last_accessed = '2017-02-01'
+
+
+# In[23]:
+
+
 def motie_headlines(motie_photo_news_url):
 
     ''' This function takes the url of the MOTIE photo news page and returns a
-    Pandas dataframe containing the headlines and urls of the stories on the
+    Pandas dataframe containing the urls of the stories on the
     page.'''
 
     # Save HTML of MOTIE URL
@@ -21,14 +37,6 @@ def motie_headlines(motie_photo_news_url):
 
     # Save soup of MOTIE photo news page
     soup = BeautifulSoup(motie_photo_news, 'html.parser')
-
-    # Save headlines of MOTIE photo news page
-    headlines_code = soup.find_all('span', attrs={'class':'tit'})
-
-    # Save each headline from MOTIE soup as item in list_of_headlines
-    for headline in headlines_code:
-
-        list_of_headlines.append(str(headline.get_text()))
 
     # Get links
     links = soup.find_all('a', attrs={'title':'Detail View'})
@@ -42,12 +50,16 @@ def motie_headlines(motie_photo_news_url):
         list_of_links.append(str('http://english.motie.go.kr/en/pc/photonews/bbs/'+link['href']))
 
     # Create dictionary of headlines and links
-    dictionary = {'Story title':list_of_headlines, 'URL':list_of_links}
+    dictionary = {'URL':list_of_links}
 
     # Create empty dataframe
     headlines_urls = pd.DataFrame(data = dictionary)
 
     return headlines_urls
+
+
+# In[24]:
+
 
 def motie_photo_news_story(story_url):
 
@@ -61,10 +73,10 @@ and returns a dataframe containing key information about that story.'''
     soup = BeautifulSoup(MOTIE_photo_story, 'html.parser')
 
     # Save story headline
-    story_headline = str(soup.find('h3').get_text())
+    story_headline = soup.find('h3').get_text()
 
     # Save story date
-    story_date = str(soup.find('span', attrs={'class':'date'}).get_text())
+    story_date = list(soup.find('h3').children)[1].get_text()
 
     # Save story author
     story_author = 'Republic of Korea Ministry of Trade, Industry and Energy'
@@ -79,6 +91,10 @@ and returns a dataframe containing key information about that story.'''
     story_dataframe = pd.DataFrame(data = story_dictionary, index = [0])
 
     return story_dataframe
+
+
+# In[25]:
+
 
 # Define MOTIE photo news URL
 motie_photo_news_url = 'http://english.motie.go.kr/en/pc/photonews/bbs/bbsList.do?bbs_cd_n=1'
@@ -102,3 +118,29 @@ stories = stories.reset_index().drop(labels = 'index', axis = 1)
 
 # Merge headlines dataframe with stories dataframe
 motie_photo_news = pd.merge(stories, motie_photo_news, left_on='URL', right_on='URL')
+
+
+# In[26]:
+
+
+fully_updated = date_last_accessed > motie_photo_news['Date'].min()
+
+
+# In[27]:
+
+
+last_url = story_URLs[-1]
+last_story_number = (story_URLs[-1])[68:71]
+
+
+# In[28]:
+
+
+while fully_updated is False:
+    
+    last_story_number = int(last_story_number) - 1
+    next_story_url = "http://english.motie.go.kr/en/pc/photonews/bbs/bbsList.do?bbs_seq_n={}&bbs_cd_n=1&currentPage=1&search_key_n=&search_val_v=&cate_n=".format(last_story_number)
+    next_story_df = motie_photo_news_story(next_story_url)
+    motie_photo_news = pd.concat([motie_photo_news,next_story_df])
+    fully_updated = date_last_accessed > motie_photo_news['Date'].min()
+
