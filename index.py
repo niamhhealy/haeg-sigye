ff95739b742babc1a8b84256d656eec69cb52c12
unknown <nfranceshealy@gmail.com>
2018-03-05 21:43:12 +0000

    changes

--- index.py
+++ index.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask("Project")
 
 @app.route("/index")
 def welcome():
     return render_template("index.html")
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 app.run(debug = True)
