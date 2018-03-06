029fb1767277886216ae96936cdd5943917e157a
unknown <nfranceshealy@gmail.com>
2018-03-06 23:24:04 +0000

    major changes

--- index.py
+++ index.py
@@ -1,31 +1,19 @@
 from flask import Flask, render_template, redirect, url_for, request
 
 import motie_photo_news as motie
+import pandas as pd
 
 app = Flask("Project")
 
-@app.route("/index")
-def welcome():
-    return render_template("index.html")
+@app.route("/access")
+def access():
+    return render_template("trial.html")
 
-# Route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
-
-# Route for handling tables
-@app.route("/tables")
-def show_tables():
-    data = motie.motie_photo_news
-    data.set_index(['Name'], inplace=True)
-    data.index.name=None
-    return render_template('view.html',table=[data.to_html],
-    titles = ['MOTIE'])
+@app.route("/test",methods=["POST"])
+def date_accessed():
+    form_data = request.form
+    date = form_data['dateaccessed']
+    data = motie.produce_motie_df(date)
+    return render_template('view.html',table=[data.to_html()])
 
 app.run(debug = True)
--- motie_photo_news.py
+++ motie_photo_news.py
@@ -7,8 +7,6 @@ import urllib2
 # Import Pandas
 import pandas as pd
 
-date_last_accessed = '2017-02-01'
-
 # Define motie headlines function
 def motie_headlines(motie_photo_news_url):
 
@@ -76,52 +74,56 @@ def motie_photo_news_story(story_url):
 
     return story_dataframe
 
-# Define MOTIE photo news URL
-motie_photo_news_url = 'http://english.motie.go.kr/en/pc/photonews/bbs/bbsList.do?bbs_cd_n=1'
+def produce_motie_df(date_last_accessed):
 
-# Run MOTIE headlines function on URL + save as new dataframe
-motie_photo_news = motie_headlines(motie_photo_news_url)
+    # Define MOTIE photo news URL
+    motie_photo_news_url = 'http://english.motie.go.kr/en/pc/photonews/bbs/bbsList.do?bbs_cd_n=1'
 
-# Create list of URLs to find more info on
-story_URLs = motie_photo_news['URL'].tolist()
+    # Run MOTIE headlines function on URL + save as new dataframe
+    motie_photo_news = motie_headlines(motie_photo_news_url)
 
-# Create empty dataframe to contain further information
-stories = pd.DataFrame(data = None)
+    # Create list of URLs to find more info on
+    story_URLs = motie_photo_news['URL'].tolist()
 
-# Create stories dataframe containing more information for each headline page
-for url in story_URLs:
-    story_dataframe = motie_photo_news_story(url)
-    stories = pd.concat([stories, story_dataframe])
+    # Create empty dataframe to contain further information
+    stories = pd.DataFrame(data = None)
 
-# Tidy up stories dataframe
-stories = stories.reset_index().drop(labels = 'index', axis = 1)
+    # Create stories dataframe containing more information for each headline page
+    for url in story_URLs:
+        story_dataframe = motie_photo_news_story(url)
+        stories = pd.concat([stories, story_dataframe])
 
-# Merge headlines dataframe with stories dataframe
-motie_photo_news = pd.merge(stories, motie_photo_news, left_on='URL', right_on='URL')
+    # Tidy up stories dataframe
+    stories = stories.reset_index().drop(labels = 'index', axis = 1)
 
-# Create variable that if true if motie news story table fully updated
-fully_updated = date_last_accessed > motie_photo_news['Date'].min()
+    # Merge headlines dataframe with stories dataframe
+    motie_photo_news = pd.merge(stories, motie_photo_news, left_on='URL', right_on='URL')
 
-# Save news story with oldest URL as string
-last_url = story_URLs[-1]
-
-# Take news story number from oldest URL
-last_story_number = (story_URLs[-1])[68:71]
-
-# create while loop to run if table not fully updated from newest 8 stories
-while fully_updated is False:
-
-    # save next news story number
-    last_story_number = int(last_story_number) - 1
-
-    # save url for next news story
-    next_story_url = "http://english.motie.go.kr/en/pc/photonews/bbs/bbsList.do?bbs_seq_n={}&bbs_cd_n=1&currentPage=1&search_key_n=&search_val_v=&cate_n=".format(last_story_number)
-
-    # run motie_photo_news_Story on next news story
-    next_story_df = motie_photo_news_story(next_story_url)
-
-    # add next news story to motie_photo_news
-    motie_photo_news = pd.concat([motie_photo_news,next_story_df])
-
-    # check whether table fully updated yet
+    # Create variable that if true if motie news story table fully updated
     fully_updated = date_last_accessed > motie_photo_news['Date'].min()
+
+    # Save news story with oldest URL as string
+    last_url = story_URLs[-1]
+
+    # Take news story number from oldest URL
+    last_story_number = (story_URLs[-1])[68:71]
+
+    # create while loop to run if table not fully updated from newest 8 stories
+    while fully_updated is False:
+
+        # save next news story number
+        last_story_number = int(last_story_number) - 1
+
+        # save url for next news story
+        next_story_url = "http://english.motie.go.kr/en/pc/photonews/bbs/bbsList.do?bbs_seq_n={}&bbs_cd_n=1&currentPage=1&search_key_n=&search_val_v=&cate_n=".format(last_story_number)
+
+        # run motie_photo_news_Story on next news story
+        next_story_df = motie_photo_news_story(next_story_url)
+
+        #  add next news story to motie_photo_news
+        motie_photo_news = pd.concat([motie_photo_news,next_story_df])
+
+        # check whether table fully updated yet
+        fully_updated = date_last_accessed > motie_photo_news['Date'].min()
+
+    return motie_photo_news
