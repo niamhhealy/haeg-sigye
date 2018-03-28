945bf72c6fcc8ff4dc6ba33847e7996dc61bcc6f
unknown <nfranceshealy@gmail.com>
2018-03-28 23:28:14 +0100

    ok

--- app.py
+++ app.py
@@ -17,5 +17,9 @@ def date_accessed():
     size = len(data['Story title'])
     return render_template('view.html',data=data,size=size)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == '__main__':
     app.run(debug = True)
